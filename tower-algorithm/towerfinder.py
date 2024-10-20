c0d348876a9e5659be665f0e69986153436f35ba
iremtaze <taze20@itu.edu.tr>
2024-10-20 16:20:42 +0300

    Updated tower distribution logic and dataset

--- tower-algorithm/towerfinder.py
+++ tower-algorithm/towerfinder.py
@@ -3,23 +3,24 @@ import pandas as pd
 file_path = './data.csv'
 df = pd.read_csv(file_path)
 
-df['var3'] = df['Destruction Rate'] * df['Population Rate']
-
-df['var6'] = df['var3'].rolling(window=5, min_periods=1).sum()
-
 df['Towers Inside'] = 0
 
-def distribute_towers(df, num_towers):
-    while num_towers > 0:
+def calculate_var6(df):
+    df['var6'] = df['Percentage'].rolling(window=5, min_periods=1).sum()
+    return df
 
-        df['var4'] = df['var6'] / (1 + df['Towers Inside'])
+def distribute_towers(df, num_towers):
+    df = calculate_var6(df)
+    while num_towers > 0:
+        df['var4'] = df['var6'] / (1 + (df['Towers Inside']))
         
         highest_var4_index = df['var4'].idxmax()
         
         df.loc[highest_var4_index, 'Towers Inside'] += 1
         num_towers -= 1
         
-        df['var4'] = df['var6'] / (1 + df['Towers Inside'])
+        df = calculate_var6(df)  
+        df['var4'] = df['var6'] / (1 + (df['Towers Inside']) ** 2)  
     
     return df
 
@@ -28,4 +29,4 @@ num_towers = 100
 result_df = distribute_towers(df, num_towers)
 
 print("Final Tower Distribution:")
-print(result_df[['District', 'Towers Inside']])
+print(result_df[['District', 'Towers Inside']].to_string(index=False))
