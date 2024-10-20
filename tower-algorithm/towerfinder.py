34b2069268eeeeffbec83d0e55236bc80ae6f220
iremtaze <taze20@itu.edu.tr>
2024-10-20 16:33:54 +0300

    Updated tower distribution logic and dataset

--- tower-algorithm/towerfinder.py
+++ tower-algorithm/towerfinder.py
@@ -3,16 +3,19 @@ import pandas as pd
 file_path = './data.csv'
 df = pd.read_csv(file_path)
 
+df = df.drop_duplicates(subset='District')
+
 df['Towers Inside'] = 0
 
 def calculate_var6(df):
-    df['var6'] = df['Percentage'].rolling(window=5, min_periods=1).sum()
+    df['var6'] = 5 * df['Percentage'] + df['Percentage'].rolling(window=5, min_periods=1).sum()
     return df
 
 def distribute_towers(df, num_towers):
     df = calculate_var6(df)
     while num_towers > 0:
-        df['var4'] = df['var6'] / (1 + (df['Towers Inside']))
+
+        df['var4'] = df['var6'] / (1 + df['Towers Inside'])
         
         highest_var4_index = df['var4'].idxmax()
         
@@ -20,13 +23,15 @@ def distribute_towers(df, num_towers):
         num_towers -= 1
         
         df = calculate_var6(df)  
-        df['var4'] = df['var6'] / (1 + (df['Towers Inside']) ** 2)  
+        df['var4'] = df['var6'] / (1 + df['Towers Inside'])  
     
     return df
 
 num_towers = 100
 
 result_df = distribute_towers(df, num_towers)
+
+result_df = result_df.drop_duplicates(subset='District')
 
 print("Final Tower Distribution:")
 print(result_df[['District', 'Towers Inside']].to_string(index=False))
